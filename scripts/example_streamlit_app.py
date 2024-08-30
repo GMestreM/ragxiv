5883a14aa5e871242369b4c360e412147e9eade1
GMestreM <gmestrem@gmail.com>
2024-08-30 18:09:29 +0200

    Add thumbs up/down to streamlit
    This code shows how to capture user feedback inside our frontend

--- scripts/example_streamlit_app.py
+++ scripts/example_streamlit_app.py
@@ -3,7 +3,8 @@
 import os
 import sys
 from dotenv import load_dotenv
-from typing import Final, Generator
+from typing import List, Final, Generator
+from datetime import datetime
 import streamlit as st
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -74,9 +75,26 @@ st.header(
 )
 
 
-# Initialize chat history and selected model
+# Initialize chat history and user feedback
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+if "feedback" not in st.session_state:
+    st.session_state.feedback = []
+
+if "question_state" not in st.session_state:
+    st.session_state.question_state = False
+
+if "response" not in st.session_state:
+    st.session_state.response = None
+
+if "user_feedback" not in st.session_state:
+    st.session_state.user_feedback = None
+
+
+def fbcb():
+    # Callback function
+    print(st.session_state.response)
 
 
 # Display chat messages from history on app rerun
@@ -86,6 +104,19 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
+def build_user_feedback(
+    user_question: str, system_answer: str, references: List[str], satisfied: bool
+) -> dict:
+    user_feedback = {
+        "timestamp": datetime.now(),
+        "user_question": user_question,
+        "answer": system_answer,
+        "references": references,
+        "satisfied": satisfied,
+    }
+    return user_feedback
+
+
 def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
     """Yield chat response content from the Groq API response."""
     for chunk in chat_completion:
@@ -93,61 +124,69 @@ def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
             yield chunk.choices[0].delta.content
 
 
-if user_query := st.chat_input("Enter your prompt here..."):
+user_query = st.chat_input("Enter your prompt here...")
+if user_query:
+    st.session_state.question_state = True
+
+if st.session_state.question_state:
     st.session_state.messages.append({"role": "user", "content": user_query})
 
     with st.chat_message("user", avatar="👨‍💻"):
         st.markdown(user_query)
 
     # Fetch response from Groq API
-    try:
-        # Search and retrieve relevant document
-        semantic_search_abstract = SemanticSearch(
-            query=user_query,
-            table=TABLE_EMBEDDING_ABSTRACT,
-            similarity_metric="<#>",
-            embedding_model=EMBEDDING_MODEL_NAME,
-            max_documents=3,
-        )
+    with st.spinner(""):
+        try:
+            # Search and retrieve relevant document
+            semantic_search_abstract = SemanticSearch(
+                query=user_query,
+                table=TABLE_EMBEDDING_ABSTRACT,
+                similarity_metric="<#>",
+                embedding_model=EMBEDDING_MODEL_NAME,
+                max_documents=3,
+            )
 
-        semantic_search_article = SemanticSearch(
-            query=user_query,
-            table=TABLE_EMBEDDING_ARTICLE,
-            similarity_metric="<#>",
-            embedding_model=EMBEDDING_MODEL_NAME,
-            max_documents=3,
-        )
+            semantic_search_article = SemanticSearch(
+                query=user_query,
+                table=TABLE_EMBEDDING_ARTICLE,
+                similarity_metric="<#>",
+                embedding_model=EMBEDDING_MODEL_NAME,
+                max_documents=3,
+            )
 
-        semantic_search_hierarchy = [semantic_search_abstract, semantic_search_article]
+            semantic_search_hierarchy = [
+                semantic_search_abstract,
+                semantic_search_article,
+            ]
 
-        relevant_documents = retrieve_similar_documents(
-            conn=conn,
-            retrieval_method=RETRIEVAL_METHOD,
-            retrieval_parameters=semantic_search_hierarchy,
-        )
-        prompt = build_rag_prompt(
-            user_question=relevant_documents["question"],
-            context=relevant_documents["documents"],
-        )
+            relevant_documents = retrieve_similar_documents(
+                conn=conn,
+                retrieval_method=RETRIEVAL_METHOD,
+                retrieval_parameters=semantic_search_hierarchy,
+            )
+            prompt = build_rag_prompt(
+                user_question=relevant_documents["question"],
+                context=relevant_documents["documents"],
+            )
 
-        chat_completion = client.chat.completions.create(
-            model="llama-3.1-70b-versatile",
-            messages=[{"role": "user", "content": prompt}],
-            stream=True,
-        )
+            chat_completion = client.chat.completions.create(
+                model="llama-3.1-70b-versatile",
+                messages=[{"role": "user", "content": prompt}],
+                stream=True,
+            )
 
-        # Define string with suggested papers
-        references_response = f"""
-        If you would like to learn more about the topic, I suggest you refer to the following papers: \n\n
+            # Define string with suggested papers
+            references_response = f"""
+            If you would like to learn more about the topic, I suggest you refer to the following papers: \n\n
 {'\n'.join([f'- {url}' for url in relevant_documents['references']])}
-    """
+        """
 
-        # Use the generator function with st.write_stream
-        with st.chat_message("assistant", avatar="🤖"):
-            chat_responses_generator = generate_chat_responses(chat_completion)
-            full_response = st.write_stream(chat_responses_generator)
-    except Exception as e:
-        st.error(e, icon="🚨")
+            # Use the generator function with st.write_stream
+            with st.chat_message("assistant", avatar="🤖"):
+                chat_responses_generator = generate_chat_responses(chat_completion)
+                full_response = st.write_stream(chat_responses_generator)
+        except Exception as e:
+            st.error(e, icon="🚨")
 
     # Append the full response to session_state.messages
     if isinstance(full_response, str):
@@ -165,4 +204,33 @@ if user_query := st.chat_input("Enter your prompt here..."):
     st.session_state.messages.append(
         {"role": "assistant", "content": references_response}
     )
-    st.rerun()
+
+    # Display chat messages from history on app rerun
+    message = st.session_state.messages[-1]
+    avatar = "🤖" if message["role"] == "assistant" else "👨‍💻"
+    with st.chat_message(message["role"], avatar=avatar):
+        st.markdown(message["content"])
+
+    # Add thumbs up / thumbs down buttons
+    response = st.feedback("thumbs", on_change=fbcb, key="response")
+
+    # Capture feedback details
+    user_feedback = build_user_feedback(
+        user_question=user_query,
+        system_answer=full_response,
+        references=relevant_documents["references"],
+        satisfied=response,
+    )
+    st.session_state.user_feedback = user_feedback
+
+    st.session_state.question_state = False
+
+
+if st.session_state.response is not None:
+    # Set correct user feedback selection
+    st.session_state.user_feedback["response"] = st.session_state.response
+    print(st.session_state.user_feedback)
+
+    # Now it can be stored in a database (or appended to a list)
+    st.session_state.feedback.append(st.session_state.user_feedback)
+    print(st.session_state.feedback)
