8ca019620236870490a86922c52d6aeb1b01f0b1
GMestreM <gmestrem@gmail.com>
2024-08-31 18:34:55 +0200

    Filter documents before generating evaluation questions

--- scripts/generate_evaluation_questions.py
+++ scripts/generate_evaluation_questions.py
@@ -31,13 +31,18 @@ LLM_MODEL_PARAMS = GroqParams(api_key=GROQ_API_KEY, model="llama-3.1-70b-versati
 
 # Load documents
 metadata = pd.read_csv(METADATA_PATH, sep=";")
+markdown_text = pd.read_csv(MARKDOWN_ARTICLES_PATH, sep=";")
+# Filter metadata by id
+metadata = metadata.loc[metadata["id"].isin(markdown_text["id"]), :]
+metadata.set_index("id", inplace=True)
 
 # Loop and build questions
 eval_questions = []
+failed_to_parse = []
 
 for idx, row in tqdm(metadata.iterrows(), total=metadata.shape[0]):
     time.sleep(2)
-    document_id = row["id"]
+    document_id = row["entry_url"]
     abstract = row["summary"]
 
     query = build_retrieval_evaluation_prompt(document=abstract, number_questions=3)
@@ -48,11 +53,17 @@ for idx, row in tqdm(metadata.iterrows(), total=metadata.shape[0]):
     # print(response["response"])
 
     # Parse response into lists
-    questions = json.loads(response["response"])
+    try:
+        questions = json.loads(response["response"])
+    except Exception as e:
+        print(document_id)
+        failed_to_parse.append(document_id)
+        questions = response["response"]
     eval_question = EvaluationQuestions(document_id=document_id, questions=questions)
     eval_questions.append(eval_question)
 
 # Store as csv file
+print(failed_to_parse)
 pd.DataFrame(eval_questions).to_csv(
     f"metadata_evaluation_questions_{metadata.shape[0]}.csv", sep=";"
 )
